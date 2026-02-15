a5ab3c50511152b192617c307921491e18fa0042
phazed <wtfux100@yahoo.com>
2026-02-15 14:48:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1900,6 +1900,12 @@ async function initApp() {
   populateFolderSelect("General");
 }
 
+// Make core things available to tool-*.js files
+window.toolsConfig = toolsConfig;
+window.renderToolPanel = renderToolPanel;
+window.renderToolsNav = renderToolsNav;
+
+
 document.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("generatorNav")
